4964991871bfdc9adc5905d4e21df33bafdf06ee
itsayushch <ayush.chowdhury2004@gmail.com>
2021-04-12 18:06:49 +0530

    hastags appear on right and only on hover

--- components/Markdown.js
+++ components/Markdown.js
@@ -4,54 +4,60 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Divider from '@material-ui/core/Divider';
+
 const styles = (theme) => ({
   listItem: {
     marginTop: theme.spacing(1),
   },
+  '@global': {
+    'h3 .header-link-class span, h4 .header-link-class span, h5 .header-link-class span': {
+      visibility: 'hidden'
+    },
+    'h3:hover .header-link-class span, h4:hover .header-link-class span, h5:hover .header-link-class span, h3 .header-link-class:focus span, h4 .header-link-class:focus span, h5 .header-link-class:focus span': {
+      visibility: 'visible'
+    }
+  }
 });
 
 const options = {
   overrides: {
     h1: {
       component: (({ children, ...props }) => (
-        <> 
+        <>
           <br />
-          <Typography variant='h3' gutterBottom {...props}><Link href={`#${props.id}`}>#</Link> {children}</Typography>
-          <Divider />  
-          <br />
+          <Typography variant='h3' className='header-link-class' gutterBottom {...props}>{children} <span><Link href={`#${props.id}`}>#</Link></span></Typography>
+          <Divider />
         </>
       ))
     },
     h2: {
       component: (({ children, ...props }) => (
-        <> 
+        <>
           <br />
-          <Typography variant='h4' gutterBottom {...props}><Link href={`#${props.id}`}>#</Link> {children}</Typography>
-          <Divider />  
-          <br />
+          <Typography variant='h4' className='header-link-class' gutterBottom {...props}>{children} <span><Link href={`#${props.id}`}>#</Link></span></Typography>
+          <Divider />
         </>
       ))
     },
     h3: {
-      component: (({ children , ...props }) => (
-        <> 
-          <Typography variant='h5' gutterBottom {...props}><Link href={`#${props.id}`}>#</Link> {children}</Typography>
+      component: (({ children, ...props }) => (
+        <>
+          <Typography variant='h5' className='header-link-class' gutterBottom {...props}>{children} <span><Link href={`#${props.id}`}>#</Link></span></Typography>
           <br />
         </>
       ))
     },
     h4: {
-      component: (({ children , ...props }) => (
-        <> 
-          <Typography variant='subtitle1' gutterBottom {...props}><Link href={`#${props.id}`}>#</Link> {children}</Typography>
-          <Divider /> 
-          <br /> 
+      component: (({ children, ...props }) => (
+        <>
+          <Typography variant='subtitle1' className='header-link-class' gutterBottom {...props}>{children} <span><Link href={`#${props.id}`}>#</Link></span></Typography>
+          <Divider />
         </>
       ))
     },
     p: { component: Typography, props: { paragraph: true, gutterBottom: true } },
     a: { component: Link },
-    li: { 
+    li: {
       component: withStyles(styles)(({ classes, ...props }) => (
         <li className={classes.listItem} key={props.id}>
           <Typography component="span" {...props} />
